885ac27bfcd8a6be39a4d7971d4b3816cb8a78ff
kamil <gkf5153@gmail.com>
2024-12-24 12:58:12 +0300

    Настроен базовый DAO

---
+++ dao/base.py
@@ -0,0 +1,33 @@
+from typing import TypeVar, Type, Optional, List
+
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import Base, async_session_maker
+
+
+class BaseDAO:
+    model = None
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().one_or_none()
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.mappings().all()
+
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**data).returning(cls.model)
+            result = await session.execute(query)
+            await session.commit()
+            return result.scalar_one_or_none()
+
