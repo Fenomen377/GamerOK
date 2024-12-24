51e4770f685513602e2deb7466f02d7545f8ed01
kamil <gkf5153@gmail.com>
2024-12-24 12:57:52 +0300

    Настроен основной файл main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 
+from bookings.router import router_booking
+from club.router import router_club
+from club.zone.router import router_zone
+from club.zone.table.router import router_table
+from users.router import router_users, router_auth
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(router_users)
+app.include_router(router_auth)
+app.include_router(router_booking)
+app.include_router(router_club)
+app.include_router(router_zone)
+app.include_router(router_table)
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
