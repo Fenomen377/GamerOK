b7ee155bd3b222466027e1abf63d3a8bb4bf6bba
kamil <gkf5153@gmail.com>
2025-01-05 15:03:11 +0300

    Внесены корректировки

--- users/dependencies.py
+++ users/dependencies.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from fastapi import HTTPException, Request, Depends
 from jose import jwt, JWTError
@@ -8,6 +9,7 @@ from config import settings
 from exceptions import TokenAbsentException, IncorrectTokenFormatException, TokenExpiredException, \
     UserIsNotPresentException
 from users.dao import UserDAO
+from users.models import Users
 
 
 def get_token(request: Request):
@@ -17,21 +19,20 @@ def get_token(request: Request):
     return token
 
 
-async def get_current_user(token: str = Depends(get_token)):
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, settings.ALGORITHM
-        )
-    except JWTError:
-        raise IncorrectTokenFormatException
-    expire: str = payload.get("exp")
-    if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise TokenExpiredException
-    user_id: str = payload.get("sub")
-    if not user_id:
-        raise UserIsNotPresentException
-    user = await UserDAO.find_one_or_none(id=int(user_id))
-    if not user:
-        raise UserIsNotPresentException
+async def get_current_user(request: Request) -> Optional[Users]:
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        return None  # Токена нет, пользователь не авторизован
 
-    return user
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        expire = payload.get("exp")
+        if not expire or int(expire) < datetime.utcnow().timestamp():
+            return None  # Токен истек, пользователь не авторизован
+        user_id = payload.get("sub")
+        if not user_id:
+            return None
+        user = await UserDAO.find_one_or_none(id=int(user_id))
+        return user
+    except JWTError:
+        return None
