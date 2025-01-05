c75e1ef864015c68f38aec1afbed45dcb472bb0d
kamil <gkf5153@gmail.com>
2025-01-05 15:02:34 +0300

    Внесены корректировки

--- database.py
+++ database.py
@@ -3,10 +3,17 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from config import settings
 
+
 engine = create_async_engine(settings.DATABASE_URL)
+
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
     pass
+
+
+async def get_db_session() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
