f8982e9eaac4387994ebfa96eb0625ac112c2465
kamil <gkf5153@gmail.com>
2025-01-05 15:03:07 +0300

    Внесены корректировки

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from bookings.router import router_booking
 from club.router import router_club
 from club.zone.router import router_zone
 from club.zone.table.router import router_table
+from pages.router import router_frontend
 from users.router import router_users, router_auth
 
 app = FastAPI()
@@ -14,6 +16,10 @@ app.include_router(router_booking)
 app.include_router(router_club)
 app.include_router(router_zone)
 app.include_router(router_table)
+app.include_router(router_frontend)
+
+
+app.mount("/static", StaticFiles(directory="static/"), "static")
 # @app.get("/")
 # async def root():
 #     return {"message": "Hello World"}
